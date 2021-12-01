42d1eec26f731062eeb290ece210d76bc312f68a
matheus rodrigues rosa <matheusrodriguesr@hotmail.com>
2021-11-30 21:03:00 -0300

    fragments e viewModels

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/model/character/CharacterModelResponse.kt
@@ -0,0 +1,10 @@
+package daniel.lop.io.marvelappstarter.data.model.character
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class CharacterModelResponse(
+
+    @SerializedName("data")
+    val data: CharacterModelData
+): Serializable
---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/model/comic/ComicModelData.kt
@@ -0,0 +1,10 @@
+package daniel.lop.io.marvelappstarter.data.model.comic
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class ComicModelData(
+
+    @SerializedName("results")
+    val result: List<ComicModel>
+):Serializable
