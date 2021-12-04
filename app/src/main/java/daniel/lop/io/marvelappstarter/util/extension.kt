1ff2f910829d50b0fae11bb47a06fb95d830f146
matheus rodrigues rosa <matheusrodriguesr@hotmail.com>
2021-12-04 19:30:46 -0300

    adapters/extensions

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/extension.kt
@@ -0,0 +1,31 @@
+package daniel.lop.io.marvelappstarter.util
+
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import java.time.Duration
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_LONG){
+    Toast.makeText(
+        requireContext(),
+        message,
+        duration
+    ).show()
+}
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.hide(){
+    visibility = View.INVISIBLE
+}
+
+fun String.limitDescription(characters: Int): String{
+    if (this.length > characters){
+        val firstCharacter = 0
+        return "${this.substring(firstCharacter, characters)}..."
+    }
+    return this
+}
+
