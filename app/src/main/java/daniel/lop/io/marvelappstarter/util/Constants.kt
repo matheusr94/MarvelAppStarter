e9b1b8bd7512c02cc37632b11479f3f603f8d8c9
matheus rodrigues rosa <matheusrodriguesr@hotmail.com>
2021-12-03 22:05:31 -0300

    injecao de dependencia

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,12 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/piblic/"
+    const val PUBLIC_KEY = "71e70f6d2f42e8c9099151eccfe6ad2b"
+    const val PRIVATE_KEY = "e3c60a2616c60dbb4cdb6aad8d7e876965ba5781"
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
