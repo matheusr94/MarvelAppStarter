6cbbb364e9aebdc38b7098f30270f833ae502225
matheus rodrigues rosa <matheusrodriguesr@hotmail.com>
2021-12-06 21:05:02 -0300

    pesquisa

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val PUBLIC_KEY = "71e70f6d2f42e8c9099151eccfe6ad2b"
     const val PRIVATE_KEY = "e3c60a2616c60dbb4cdb6aad8d7e876965ba5781"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
